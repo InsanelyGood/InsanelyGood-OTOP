9fc0c5e840f3b00fad6830459c47ef7975bf026e
Non <non9441@gmail.com>
2018-09-18 21:20:42 +0700

    try react router browse new page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Page from './newPage'
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+        <Route exact path="/" component={App}/>
+        <Route path="/newpage" component={Page}/>
+        </div>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
