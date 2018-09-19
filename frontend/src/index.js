96aefb96d21477648797becd0769d1dff80fc850
dacharat <dacharat2013@gmail.com>
2018-09-19 17:47:40 +0700

    :fireworks: edit project structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Page from './newPage'
+import './css/index.css';
+import App from './views/App';
+import ProductsPage from './views/products_page'
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
         <div>
-        <Route exact path="/" component={App}/>
-        <Route path="/newpage" component={Page}/>
+            <Route exact path="/" component={App} />
+            <Route path="/products" component={ProductsPage} />
         </div>
     </Router>,
     document.getElementById('root'));
