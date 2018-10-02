c617482e0b84df0b0cd6a90a0ea0b5350b090f8f
Kwankaew Uttama <kwankaew.u@ku.th>
2018-10-02 13:58:51 +0700

    init carousal

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/common/navbar'
+import CarousalHighlight from '../components/carousel_home/CarousalHighlight';
 
 class Home extends React.Component {
     render = () => {
         return (<div>
             <Navbar />
+            <CarousalHighlight />
         </div>)
     }
 }
