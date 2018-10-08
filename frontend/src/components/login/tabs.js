002ed291096a6eadb4f109459358c924377b826f
Pimwalun <janny25522009@hitmail.com>
2018-10-08 20:00:56 +0700

    edit regis page

--- frontend/src/components/login/tabs.js
+++ frontend/src/components/login/tabs.js
@@ -5,23 +5,35 @@ import LoginComponent from '../../components/login/login_component'
 import RegisComponent from '../../components/login/regis_component'
 import styled from 'styled-components'
 
-// const Form = styled.div`
+const Form = styled.div`
     
-//     // padding: 5% 37% 20% 30%;
-// `
+    // padding: 5% 37% 20% 30%;
+`
 const LoginContent = styled.div`
     padding: 5em;
     margin: auto;
+    a {
+        color: black;
+    },
+    a:hover {
+        color: black;
+    }
 `
 
-export default class Tabs extends React.Component {
+export default class Example extends React.Component {
     constructor(props) {
         super(props);
 
         this.toggle = this.toggle.bind(this);
         this.state = {
-            activeTab: '2'
+            activeTab: '1'
         };
+    }
+
+    componentDidMount() {
+        if(this.props.defaultTab === '2') {
+            this.setState({activeTab: '2'})
+        }
     }
 
     toggle(tab) {
@@ -33,10 +45,14 @@ export default class Tabs extends React.Component {
     }
     render() {
         return (
-            <LoginContent className='col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-3'>
+            <LoginContent className='col-sm-8 col-sm-offset-2 col-md-5 col-md-offset-3'>
+                <Form className='container'>
+                
+
                     <Nav tabs>
                         <NavItem>
                             <NavLink xs="6"
+                                href='/users/login'
                                 className={classnames({ active: this.state.activeTab === '1' })}
                                 onClick={() => { this.toggle('1'); }}
                             >
@@ -45,6 +61,7 @@ export default class Tabs extends React.Component {
                         </NavItem>
                         <NavItem>
                             <NavLink xs="6"
+                                href='/users/register'
                                 className={classnames({ active: this.state.activeTab === '2' })}
                                 onClick={() => { this.toggle('2'); }}
                             >
@@ -67,7 +84,9 @@ export default class Tabs extends React.Component {
                                 </Col>
                             </Row>
                         </TabPane>
-                    </TabContent>               
+                    </TabContent>
+                
+                </Form>
             </LoginContent>
         );
     }
