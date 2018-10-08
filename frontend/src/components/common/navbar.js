3c12f6e57df925644e44b52dabbfc2d6266bf37e
jiranan-pat <jiranan.pat@ku.th>
2018-10-08 13:16:37 +0700

    Add footer

--- frontend/src/components/common/navbar.js
+++ frontend/src/components/common/navbar.js
@@ -1,44 +1,67 @@
 import React from 'react'
-import {Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink} from 'reactstrap'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap'
 import '../../css/navbar.css'
 
 class NavBar extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isOpen: false
-        };
-    }
+    this.state = {
+      isOpen: false
+    };
+  }
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    render = () => {
-        return (<div>
-            <Navbar light expand="md">
-                <NavbarBrand href="/">OTOPaholic</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href='/search'>Search</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/products">Product</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/users/login">Login</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>);
-    }
+  render = () => {
+    return (<div>
+      <Navbar dark="dark" expand="md">
+        <NavbarBrand href="/">OTOPaholic</NavbarBrand>
+        <NavbarToggler onClick={this.toggle}/>
+        <Collapse isOpen={this.state.isOpen} navbar="navbar">
+          <Nav className="ml-auto" navbar="navbar">
+            <NavItem>
+              <NavLink href='/north'>north</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/central'>central</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/south'>south</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/northeast'>northeast</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/products">all</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/search'>Search<i class="fas fa-search"></i></NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/cart'>cart</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Login</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>);
+  }
 }
 
 export default NavBar
