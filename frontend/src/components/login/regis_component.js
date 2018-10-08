01a98fe286a17a58561e2176bc5848441df2ca4b
Pimwalun <janny25522009@hitmail.com>
2018-10-08 13:42:26 +0700

    edit login

--- frontend/src/components/login/regis_component.js
+++ frontend/src/components/login/regis_component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Input, Label } from 'reactstrap';
+import { Input, Label , FormFeedback} from 'reactstrap';
 import styled from 'styled-components'
 
 const Form = styled.div`
@@ -32,17 +32,46 @@ class LoginComponent extends React.Component {
         this.state = {
             email: '',
             password: '',
+            comfirmPassword: '',
+            firstName: '',
+            lastName: '',
+            contactNumber: '',
+            error: '',
         };
+        this.handlePassChange = this.handlePassChange.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.dismissError = this.dismissError.bind(this);
     }
 
-    handleChange = event => {
+    dismissError() {
+        this.setState({ error: '' });
+    }
+
+    handleUserChange = event => {
         this.setState({
-            [event.target.id]: event.target.value
+            username: event.target.value,
+        });
+    };
+
+    handlePassChange = event => {
+        this.setState({
+            password: event.target.value
         });
     }
 
-    onLoginClick = event => {
+    handleSubmit = event => {
         event.preventDefault();
+
+        if (!this.state.username) {
+            return this.setState({ error: 'Username is required' });
+        }
+
+        if (!this.state.password) {
+            return this.setState({ error: 'Password is required' });
+        }
+
+        return this.setState({ error: '' });
     }
 
 
@@ -53,27 +82,118 @@ class LoginComponent extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <Form >
                         <FormGroups>
+                            <Label>Username</Label>
+                            {
+                                this.state.username &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="text" name="email" placeholder="Username" />
+                                    <FormFeedback>Please enter username.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="text" name="email" placeholder="Username" />
+                                </alert>
+                            }                        </FormGroups>
+                        <FormGroups>
                             <Label>Email</Label>
-                            <Input type="email" name="email" placeholder="Email" />
+                            {
+                            this.state.error &&
+                            <alert onClick={this.dismissError}>
+                                <Input invalid type="email" name="email" placeholder="Email" />
+                                <FormFeedback>Please enter email.</FormFeedback>
+                            </alert>
+                        }
+                        {
+                            !this.state.error &&
+                            <alert onSubmit={this.dismissError}>
+                                <Input type="email" name="email" placeholder="Email" />
+                            </alert>
+                        }
+                            
                         </FormGroups>
                         <FormGroups>
                             <Label>Password</Label>
-                            <Input type="password" name="password" placeholder="Password" />
+                            {
+                                this.state.error &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="password" name="password" placeholder="Password" />
+                                    <FormFeedback>Please enter password.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="password" name="password" placeholder="Password" />
+                                </alert>
+                            }                        </FormGroups>
+                        <FormGroups>
+                            <Label>Confirm Password</Label>
+                            {
+                                this.state.error &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="password" name="password" placeholder="Password" />
+                                    <FormFeedback>Please enter password.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="password" name="cPassword" placeholder="Confirm Password" />
+                                </alert>
+                            }  
                         </FormGroups>
                         <FormGroups>
                             <Label>Firstname</Label>
-                            <Input type="text" name="firstName" id="id_fn" placeholder="Firstname" />
+                            {
+                                this.state.error &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="text" name="firstName" id="id_fn" placeholder="Firstname" />
+                                    <FormFeedback>Please enter first name.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="text" name="firstName" id="id_fn" placeholder="Firstname" />
+                                </alert>
+                            } 
                         </FormGroups>
                         <FormGroups>
                             <Label>Lastname</Label>
-                            <Input type="text" name="lastName" id="id_ln" placeholder="Lastname" />
+                            {
+                                this.state.error &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="text" name="lastName" id="id_ln" placeholder="Lastname" />
+                                    <FormFeedback>Please enter last name.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="text" name="lastName" id="id_ln" placeholder="Lastname" />
+                                </alert>
+                            }
                         </FormGroups>
                         <FormGroups>
                             <Label>Contact Number</Label>
-                            <Input type="text" name="contactNumber" id="id_cn" placeholder="Contact Number" />
+                            {
+                                this.state.error &&
+                                <alert onClick={this.dismissError}>
+                                    <Input invalid type="text" name="contactNumber" id="id_cn" placeholder="Contact Number" />
+                                    <FormFeedback>Please enter contact number.</FormFeedback>
+                                </alert>
+                            }
+                            {
+                                !this.state.error &&
+                                <alert onSubmit={this.dismissError}>
+                                    <Input type="text" name="contactNumber" id="id_cn" placeholder="Contact Number" />
+                                </alert>
+                            }
                         </FormGroups>
                         <FormGroups>
-                            <Buttons type="submit" value='Signup' />
+                            <Buttons type="submit" value='Sign up' />
                         </FormGroups>
                     </Form>
                 </form>
