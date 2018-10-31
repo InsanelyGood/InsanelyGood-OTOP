6708cd2a9cae6b3ddfd37ae3bdd1d3580107cedf
dacharat <dacharat2013@gmail.com>
2018-10-31 08:59:04 +0700

    :fireworks: pretty code

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,54 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
-let User = require('../models/users');
+let User = require("../models/users");
 
 // Login Form
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function(req, res) {
+  res.render("login");
 });
 
-router.get('/test', (req, res) => {
-  console.log('lsdbackdj-------------------------------------')
+router.get("/test", (req, res) => {
+  console.log("lsdbackdj-------------------------------------");
   // console.log(req.cookies)
   // res.sendStatus(200)
-  res.status(200).send("Hello world")
-})
+  res.status(200).send("Hello world");
+});
 
 // Login Process
-router.post('/login', function (req, res, next) {
+router.post("/login", function(req, res, next) {
   console.log("login");
 
-  passport.authenticate('local', function (err, user, info) {
-    console.log('in local authen')
+  passport.authenticate("local", function(err, user, info) {
+    console.log("in local authen");
     if (err) {
-      console.log('in error')
-      console.log(err)
+      console.log("in error");
+      console.log(err);
       return next(err);
     }
 
-    console.log('user', user)
+    console.log("user", user);
     if (!user) {
-      return res.redirect('http://localhost:3000/users/login')
+      return res.redirect("http://localhost:3000/users/login");
     }
 
-    req.logIn(user, function (err) {
-      if (err) { return next(err); }
-      return res.cookie('username', user.username).redirect('http://localhost:3000/');
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res
+        .cookie("username", user.username)
+        .redirect("http://localhost:3000/");
     });
   })(req, res, next);
 });
 
 // Register Form
-router.get('/register', function (req, res) {
-  res.render('register');
-})
+router.get("/register", function(req, res) {
+  res.render("register");
+});
 
 // Register Process
-router.post('/register', function (req, res) {
-
+router.post("/register", function(req, res) {
   const firstname = req.body.firstname;
   const lastname = req.body.lastname;
   const email = req.body.email;
@@ -57,19 +60,21 @@ router.post('/register', function (req, res) {
   const confirm_password = req.body.confirm_password;
   const telephone_number = req.body.telephone_number;
 
-  req.checkBody('firstname', 'Firstname is Required').notEmpty();
-  req.checkBody('lastname', 'Lastname is Required').notEmpty();
-  req.checkBody('email', 'Email is Required').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('username', 'Username is Required').notEmpty();
-  req.checkBody('password', 'Password is Required').notEmpty();
-  req.checkBody('confirm_password', 'Confirm Password do not match').equals(req.body.password);
-  req.checkBody('telephone_number', 'telephone_number is Required').notEmpty();
+  req.checkBody("firstname", "Firstname is Required").notEmpty();
+  req.checkBody("lastname", "Lastname is Required").notEmpty();
+  req.checkBody("email", "Email is Required").notEmpty();
+  req.checkBody("email", "Email is not valid").isEmail();
+  req.checkBody("username", "Username is Required").notEmpty();
+  req.checkBody("password", "Password is Required").notEmpty();
+  req
+    .checkBody("confirm_password", "Confirm Password do not match")
+    .equals(req.body.password);
+  req.checkBody("telephone_number", "telephone_number is Required").notEmpty();
   // req.checkBody('telephone_number','telephone_number size must be 10').size() == 10;
 
   let errors = req.validationErrors();
   if (errors) {
-    console.log(errors)
+    console.log(errors);
     // res.render('register', {
     //   errors: errors
     // });
@@ -86,36 +91,32 @@ router.post('/register', function (req, res) {
       cart_list: []
     });
 
-    bcrypt.genSalt(10, function (err, salt) {
-      if (err) console.log(err)
-      bcrypt.hash(newUser.password, salt, function (err, hash) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) console.log(err);
+      bcrypt.hash(newUser.password, salt, function(err, hash) {
         if (err) {
           console.log(err);
         }
         newUser.password = hash;
-        newUser.save(function (err) {
+        newUser.save(function(err) {
           if (err) {
             console.log(err);
             return;
           } else {
-            res.redirect('http://localhost:3000/users/login');
+            res.redirect("http://localhost:3000/users/login");
           }
         });
       });
     });
-
   }
-
-})
+});
 
 // User Information
 router.get("/information", (req, res, next) => {
-  console.log('COOKIEEEEEEEEE', req.cookies)
+  console.log("COOKIEEEEEEEEE", req.cookies);
   User.find({ username: req.cookies.username }, (err, userinfo) => {
-    if (userinfo.length > 0)
-      res.status(200).send(userinfo[0]);
-    else 
-      res.status(404).send('Not found')
+    if (userinfo.length > 0) res.status(200).send(userinfo[0]);
+    else res.status(404).send("Not found");
   });
 });
 
