4a7c2368649d1f40d7bab3297052b29ecccf3e06
dacharat <dacharat2013@gmail.com>
2018-10-31 09:31:35 +0700

    :fireworks: add form for add cart

--- frontend/src/components/product_detail/product_description.js
+++ frontend/src/components/product_detail/product_description.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const Name = styled.div`
-
-`
-const Description = styled.div`
-
-`
-const Price = styled.div`
-
-`
-const ToCart = styled.button`
-    
-`
+const Name = styled.div``;
+const Description = styled.div``;
+const Price = styled.div``;
+const HiddenInput = styled.input`
+  display: none;
+`;
 
 class ProductDescription extends React.Component {
-    render = () => {
-        return (<div>
-            <Name>Product: {this.props.productDetail.name}</Name>
-            <Description>Description: {this.props.productDetail.description}</Description>
-            <Price>Price: {this.props.productDetail.price}$</Price>
-            <ToCart>Add to Cart</ToCart>
-        </div>)
-    }
+  render = () => {
+    return (
+      <div>
+        <Name>Product: {this.props.productDetail.name}</Name>
+        <Description>
+          Description: {this.props.productDetail.description}
+        </Description>
+        <Price>Price: {this.props.productDetail.price}$</Price>
+        <form action="http://localhost:8000/users/cart" method="POST">
+          <HiddenInput type="text" value={this.props.productDetail.id} />
+          <input type="number" value={1} name="quantity" />
+          <input type="submit" value="Add to Cart" />
+        </form>
+      </div>
+    );
+  };
 }
 
-export default ProductDescription
+export default ProductDescription;
