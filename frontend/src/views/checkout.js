3f9402d5ec6cbbc9e379918b7be36bd16bda1c82
Pimwalun <janny25522009@hitmail.com>
2018-11-05 19:38:28 +0700

    design checkout page

--- frontend/src/views/checkout.js
+++ frontend/src/views/checkout.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import CheckoutComponent from '../components/checkout/checkout_component'
+import styled from 'styled-components'
+import '../css/purchase.css'
 
+const Head = styled.div`
+    background-color: black;
+    width: inherit;
+    height: 70px;
+    
+`
+const Text = styled.h1`
+    font-weight: bold;
+    color: white;
+    padding-left: 20px;
+    padding-top: 10px;
+    text-align: center;
 
+`
 
 class Checkout extends Component{
     render(){
         return <div>
+            <br/>
+            <br/>
+            <Head>
+                <Text>CHECKOUT</Text>
+            </Head>
             <CheckoutComponent/>
           </div>;
     }
