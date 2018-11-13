d86a7eb7ffa70c755b27cae7f3c2274639401b9a
Kwankaew Uttama <kwankaew.u@ku.th>
2018-11-13 15:00:54 +0700

    create new carousel

--- frontend/src/views/userInfo.js
+++ frontend/src/views/userInfo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import UserInfomation from '../components/user_information/userInfo'
+import '../css/userInfo.css'
 
 class UserInfo extends Component{
     render(){
         return <div>
-            <div className='container' align="center">
+            <div className='container userComponent' align="center" >
             <UserInfomation />
             </div>
           </div>
--- frontend/src/views/userPassword.js
+++ frontend/src/views/userPassword.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import UserPassword from '../components/user_information/userPassword'
+import '../css/userInfo.css'
 
 class UserInfo extends Component{
     render(){
         return <div>
-            <div className='container' align="center">
+            <div className='container userComponent' align="center">
             <UserPassword />
             </div>
           </div>
