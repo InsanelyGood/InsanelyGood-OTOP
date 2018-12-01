a972e14ad5c72d1a03092b0037be4a12967361f3
dacharat <dacharat2013@gmail.com>
2018-12-01 11:51:19 +0700

    :fireworks: add case for config is undifined

--- backend/app.js
+++ backend/app.js
@@ -1,88 +1,93 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const passport = require('passport')
-const expressValidator = require('express-validator');
-// const configdb = require('./config/database');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const passport = require("passport");
+const expressValidator = require("express-validator");
+const configdb = require("./config/database");
 
-const config = require('./config.js')
+const config = require("./config.js");
 
 var app = express();
 
 // Connect to database
-console.log("db",config.database)
-mongoose.connect(config.database, {
-  useNewUrlParser: true
-})
-var db = mongoose.connection
-db.once('open' ,() => console.log('connected to MongoDB.'))
-db.on('error', console.error.bind(console, 'connection error:'))
+console.log("db", config.database ? config.database : configdb.database);
+mongoose.connect(
+  configdb.database,
+  {
+    useNewUrlParser: true
+  }
+);
+var db = mongoose.connection;
+db.once("open", () => console.log("connected to MongoDB."));
+db.on("error", console.error.bind(console, "connection error:"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(logger('dev'));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Express Validator Middleware
-app.use(expressValidator({
-  errorFormatter: function (param, msg, value) {
-    var namespace = param.split('.')
-      , root = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while (namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param: formParam,
-      msg: msg,
-      value: value
-    };
-  }
-}));
+  })
+);
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Route files
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productRouter = require('./routes/products');
-var orderRouter = require('./routes/orders');
-var cartRouter = require('./routes/cart');
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productRouter);
-app.use('/orders', orderRouter);
-app.use('/users/cart', cartRouter);
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var productRouter = require("./routes/products");
+var orderRouter = require("./routes/orders");
+var cartRouter = require("./routes/cart");
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/products", productRouter);
+app.use("/orders", orderRouter);
+app.use("/users/cart", cartRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
