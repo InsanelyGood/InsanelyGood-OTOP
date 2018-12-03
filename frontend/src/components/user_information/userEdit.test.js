e1a636fc9a6467cc704288482cf98add80c2bfbb
dacharat <dacharat2013@gmail.com>
2018-12-04 03:16:24 +0700

    :fireworks: fixed test fail

--- frontend/src/components/user_information/userEdit.test.js
+++ frontend/src/components/user_information/userEdit.test.js
@@ -3,8 +3,14 @@ import { mount } from "enzyme";
 import UserEdit from "./userEdit";
 
 describe("User edit component", () => {
-    const wrapper = mount(<UserEdit />);
-    it("renders without crashing (No props)", () => {
-        expect(wrapper.find('div[className="container locate col-sm-8 col-sm-offset-2 col-md-5 col-md-offset-3 "]').exists()).toEqual(true);
-      });
-})
+  const wrapper = mount(<UserEdit />);
+  it("renders without crashing (No props)", () => {
+    expect(
+      wrapper
+        .find(
+          'div[className="container locate col-sm-4 col-sm-offset-2 col-md-7 col-md-offset-3"]'
+        )
+        .exists()
+    ).toEqual(true);
+  });
+});
--- frontend/src/components/user_information/userInfo.js
+++ frontend/src/components/user_information/userInfo.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import {Button} from 'reactstrap';
-import '../../css/userInfo.css'
-import { getUsername } from '../../api/user_infomation'
-import Cookies from 'js-cookie';
-
+import React from "react";
+import { Button } from "reactstrap";
+import "../../css/userInfo.css";
+import { getUsername } from "../../api/user_infomation";
+import Cookies from "js-cookie";
 
 class UserInfomation extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      firstName: '',
-      lastName: '',
-      telephoneNumber: '',
-      address: ''
+      email: "",
+      firstName: "",
+      lastName: "",
+      telephoneNumber: "",
+      address: ""
     };
   }
 
   async componentDidMount() {
-    const info = await getUsername(Cookies.get('username'))
+    const info = await getUsername(Cookies.get("username"));
     this.setState({
       username: info.username,
       firstName: info.firstname,
@@ -27,48 +26,88 @@ class UserInfomation extends React.Component {
       email: info.email,
       telephoneNumber: info.telephoneNumber,
       address: info.address
-    })
+    });
   }
   render() {
     return (
-        <div className=" col-sm-4 col-sm-offset-2 col-md-10 col-md-offset-3 ">
-                <div className="info">
-                  <br />
-                  <p>
-                    <label>Firstname</label>
-                    <input className="boxInput" type="text" name="firstName" value={this.state.firstName} readOnly="readonly"></input>
-                  </p>
-                  <br />
-                  <p>
-                    <label>Lastname</label>
-                    <input className="boxInput" type="text" name="lastName" value={this.state.lastName} readOnly="readonly"></input>
-                  </p>
-                  <br />
-                  <p>
-                    <label>Username</label>
-                    <input className="boxInput" type="text" name="username" value={this.state.username} readOnly="readonly"></input>
-                  </p>
-                  <br />
-                  <p>
-                    <label>Address(Default)</label>
-                    <textarea type="text" name="address" value={this.state.address} readOnly="readonly"></textarea>
-                  </p>
-                  <br />
-                  <p>
-                    <label>Email</label>
-                    <input className="boxInput" type="email" name="email" value={this.state.email} readOnly="readonly"></input>
-                  </p>
-                  <br />
-                  <p>
-                    <label>Contact Number</label>
-                    <input className="boxInput" type="text" name="telephoneNumber" value={this.state.telephoneNumber} readOnly="readonly"></input>
-                  </p>
-                </div>
-                <br />
-                <p>
-                  <Button className="buttonEdit" href="http://localhost:3000/users/information/edit"><span>Edit Profile</span></Button>
-                </p> 
-        </div>  
+      <div className="col-sm-4 col-sm-offset-2 col-md-10 col-md-offset-3">
+        <div className="info">
+          <br />
+          <p>
+            <label>Firstname</label>
+            <input
+              className="boxInput"
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              readOnly="readonly"
+            />
+          </p>
+          <br />
+          <p>
+            <label>Lastname</label>
+            <input
+              className="boxInput"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              readOnly="readonly"
+            />
+          </p>
+          <br />
+          <p>
+            <label>Username</label>
+            <input
+              className="boxInput"
+              type="text"
+              name="username"
+              value={this.state.username}
+              readOnly="readonly"
+            />
+          </p>
+          <br />
+          <p>
+            <label>Address(Default)</label>
+            <textarea
+              type="text"
+              name="address"
+              value={this.state.address}
+              readOnly="readonly"
+            />
+          </p>
+          <br />
+          <p>
+            <label>Email</label>
+            <input
+              className="boxInput"
+              type="email"
+              name="email"
+              value={this.state.email}
+              readOnly="readonly"
+            />
+          </p>
+          <br />
+          <p>
+            <label>Contact Number</label>
+            <input
+              className="boxInput"
+              type="text"
+              name="telephoneNumber"
+              value={this.state.telephoneNumber}
+              readOnly="readonly"
+            />
+          </p>
+        </div>
+        <br />
+        <p>
+          <Button
+            className="buttonEdit"
+            href="http://localhost:3000/users/information/edit"
+          >
+            <span>Edit Profile</span>
+          </Button>
+        </p>
+      </div>
     );
   }
 }
