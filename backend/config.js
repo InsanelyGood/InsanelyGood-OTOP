20f1e89accda762b04a73a11bd77fe0d2c43f1ca
aommoaGitHub <vittunyuta@gmail.com>
2018-12-03 15:13:29 +0700

    fix cors backend

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,8 @@
 const config = {
+  FRONTEND_URL: {
+    global: "http://68.183.236.161:3000",
+    local: "http://localhost:3000"
+  },
   local: {
     database: "mongodb://localhost:27017/otopaholicDBTest"
   },
