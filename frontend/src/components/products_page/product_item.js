32024e0dedf74d652abc6702911f04c55f16e193
Non <non9441@gmail.com>
2018-12-03 21:31:52 +0700

    buy now button at productpage :sparkles:

--- frontend/src/components/products_page/product_item.js
+++ frontend/src/components/products_page/product_item.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import {Button} from 'reactstrap'
+import Cookies from "js-cookie";
+import { addCartItem } from '../../api/user_cart'
 
 const Box = styled.div`
   margin: 20px;
@@ -20,23 +23,31 @@ const Name = styled.div`
   text-transform: uppercase;
   margin-bottom: 1px;
 `
-const Detail = styled.p`
-  overflow-wrap: break-word;
-  margin-bottom: 1px;
+const Line = styled.div`
+  display: flex;
 `
+const Btn = styled.div`
+  margin-left: 21%;
+`
+
 const Price = styled.p`
   margin-bottom: 2px;
 `
 
 class ProductItem extends React.Component {
 
-  onMouseOver = ()=> {
-    
-  }
-
-  onMouseOut = ()=> {
-
-  }
+  handleCartAdd = async e => {
+    e.preventDefault();
+    const data = {
+      quantity: 1,
+      productID: this.props.product.id,
+      username: Cookies.get("username")
+    };
+    const res = await addCartItem(data);
+    if (res === 200) {
+      window.location.href = "/checkout";
+    }
+  };
 
     render = () => {
         return (<Link to={"/products/" + this.props.product.id}>
@@ -45,8 +56,13 @@ class ProductItem extends React.Component {
           <center><Img alt="error" src={this.props.product.image}/></center>
           <Des>
             <Name>{this.props.product.name}</Name>
-            <Detail>{this.props.product.description}</Detail>
+            <Line>
             <Price>Price: <span>{this.props.product.price}</span></Price>
+            <br/>
+            <Btn>
+            <Button color="success" size="sm" onClick={this.handleCartAdd}>Buy Now</Button>
+            </Btn>
+            </Line>
           </Des>
         </Box></Link>)
     }
